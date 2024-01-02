58e1cec73f9fcae44578cf473bf1d1eadbac7f40
Shreyas Ashtamkar <51398602+Shreyas-Ashtamkar@users.noreply.github.com>
2024-01-02 16:35:18 +0530

    Fixed Maps, -Leaflet +OMS

--- js/front.js
+++ js/front.js
@@ -63,13 +63,6 @@ $(function () {
         }
     });
 
-
-    /* =========================================
-     * Leflet map
-     *  =======================================*/
-    map();
-
-
     /* =========================================
      * parallax
      *  =======================================*/
@@ -240,81 +233,3 @@ $(function () {
     });
 
 });
-
-
-
-/* =========================================
- * styled Leaflet Map
- *  =======================================*/
-// ------------------------------------------------------ //
-// styled Leaflet  Map
-// ------------------------------------------------------ //
-
-function map() {
-
-    var mapId = 'map',
-        mapCenter = [18.622011, 73.777637],
-        mapMarker = true;
-
-    if ($('#' + mapId).length > 0) {
-
-        var icon = L.icon({
-            iconUrl: 'img/marker.png',
-            iconSize: [25, 37.5],
-            popupAnchor: [0, -18],
-            tooltipAnchor: [0, 19]
-        });
-
-        var dragging = false,
-            tap = false;
-
-        if ($(window).width() > 700) {
-            dragging = true;
-            tap = true;
-        }
-
-        var map = L.map(mapId, {
-            center: mapCenter,
-            zoom: 13,
-            dragging: dragging,
-            tap: tap,
-            scrollWheelZoom: false
-        });
-
-        // var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-        //     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        //     subdomains: ['abcd'],
-        //     minZoom: 0,
-        //     maxZoom: 20,
-        //     ext: 'png'
-        // });
-
-        var Stamen_TonerLite = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png	', {
-            attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> <a href="https://stamen.com/" target="_blank">&copy; Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
-            // subdomains: ['abcd'],
-            minZoom: 10,
-            maxZoom: 18,
-            ext: 'png'
-        });
-
-        Stamen_TonerLite.addTo(map);
-
-        map.once('focus', function () {
-            map.scrollWheelZoom.enable();
-        });
-
-        if (mapMarker) {
-            var marker = L.marker(mapCenter, {
-                icon: icon
-            }).addTo(map);
-
-            marker.bindPopup("<div class='p-4'><h5>Info Window Content</h5><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></div>", {
-                minwidth: 200,
-                maxWidth: 600,
-                className: 'map-custom-popup'
-            })
-
-        }
-    }
-
-}
